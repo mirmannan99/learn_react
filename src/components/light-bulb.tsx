94b872c45e904b7adde614b5000481e4e7fa9691
mirmannan99 <mirmannan99@gmail.com>
2025-07-26 12:40:07 +0530

    intructions added

--- src/components/light-bulb.tsx
+++ src/components/light-bulb.tsx
@@ -1,5 +1,6 @@
-type Props = {}
-
-export default function LightBulb({}: Props) {
+export default function LightBulb() {
+  //create a button to turn the ligth on off
+  // the button for on/off should be same  and should change color to green when turned on and to red when turned off
+  // also the text for the button should change
   return <div>light-bulb</div>
 }
